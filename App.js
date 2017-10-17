3970fdf9206dddee46bcc4a2ed9813cbcbeabdec
Phoebe Pan <phoebe.pan@yuelaobank.com>
2017-10-17 17:17:22 +0800

    feat: react-native + styled-components practice Done!

--- App.js
+++ App.js
@@ -1,45 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import styled from 'styled-components/native';
-import Login from './Login';
+import Movie from './Movie';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props); // (2)
-    this.handleSubmitName = this.onSubmitName.bind(this); // (3)
-    this.state = { // (4)
-      hasName: false
-    };
-  }
-
-  onSubmitName(e) { // (5)
-    const name = e.nativeEvent.text;
-    this.setState({
-      name,
-      hasName: true
-    });
-  }
 
   render() {
     return (
-      <StyledView>
-        <Login onSubmitName={ this.handleSubmitName } />
-        <StyledText>Open up App.js to start working on your app!</StyledText>
-        <StyledText>Changes you make will automatically reload.</StyledText>
-        <StyledText>Shake your phone to open the developer menu.</StyledText>
-      </StyledView>
+      <Movie />
     );
   }
 }
-
-const StyledView = styled.View`
-  flex: 1;
-  background-color: papayawhip;
-  align-items: center;
-  justify-content: center;
-`;
-
-const StyledText = styled.Text`
-  color: palevioletred;
-`;
-
---
+++ Movie.js
@@ -0,0 +1,111 @@
+import React, {
+  Component,
+} from 'react';
+import {
+  AppRegistry,
+  Image,
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import styled from 'styled-components/native';
+
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+export default class Movie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+    );
+  }
+
+  renderLoadingView() {
+    return (
+      <ContainerView>
+        <Text>
+          Loading movies...
+        </Text>
+      </ContainerView>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <ContainerView>
+        <ThumbnailImage
+          source={{uri: movie.posters.thumbnail}}
+        />
+        <RightContainerView>
+          <TitleText>{movie.title}</TitleText>
+          <Text style={styles.year}>{movie.year}</Text>
+          
+        </RightContainerView>
+      </ContainerView>
+    );
+  }
+}
+
+const ContainerView = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: #F5FCFF;
+`
+const ThumbnailImage = styled.Image`
+  width: 53;
+  height: 81;
+`
+const RightContainerView = styled.View`
+  flex: 1;
+`
+const TitleText = styled.Text`
+  font-size: 20;
+  margin-bottom: 8;
+  text-align: center;
+`
+
+var styles = StyleSheet.create({
+  year: {
+    textAlign: 'center',
+  }
+});
