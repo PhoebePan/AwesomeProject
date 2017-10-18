af59763170c985cbb72d78afb3073e0da3ee7b97
Phoebe Pan <phoebe.pan@yuelaobank.com>
2017-10-18 18:13:23 +0800

    add Hello Navigation

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Movie from './Movie';
+import { StackNavigator } from 'react-navigation';
+import styled from 'styled-components/native';
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    return <HelloText>Hello, Navigation!</HelloText>;
+  }
+}
+
+const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen }
+});
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <Movie />
+      <SimpleApp />
     );
   }
 }
+
+const HelloText = styled.Text`
+  flex: 1;
+  font-size: 36;
+  background-color: #FFFFFF;
+  align-items: center;
+  justify-content: center;
+`
