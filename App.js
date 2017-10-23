74aaa65a82beea86fb648a3c38cb2e472e22c600
Phoebe Pan <phoebe.pan@yuelaobank.com>
2017-10-23 17:48:37 +0800

    feat: add Login Logout & firebase

--- App.js
+++ App.js
@@ -1,45 +1,128 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import styled from 'styled-components/native';
-import Login from './Login';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+import { Input } from './components/Input';
+import { Button } from './components/Button';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props); // (2)
-    this.handleSubmitName = this.onSubmitName.bind(this); // (3)
-    this.state = { // (4)
-      hasName: false
-    };
+  state = {
+    email: '',
+    password: '',
+    authenticating: false,
+    user: null,
+    error: '',
   }
 
-  onSubmitName(e) { // (5)
-    const name = e.nativeEvent.text;
+  componentWillMount() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyDluZRqZwmKvqtlKkcwBdhUwzMFFNo5lkE',
+      authDomain: 'react-native-project-5d00f.firebaseapp.com',
+    }
+
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  onPressSignIn() {
     this.setState({
-      name,
-      hasName: true
+      authenticating: true,
     });
+
+    const { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => this.setState({
+        authenticating: false,
+        user,
+        error: '',
+      }))
+      .catch(() => {
+        // Login was not successful
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => this.setState({
+            authenticating: false,
+            user,
+            error: '',
+          }))
+          .catch(() => this.setState({
+            authenticating: false,
+            user: null,
+            error: 'Authentication Failure',
+          }))
+      })
+  }
+
+  onPressLogOut() {
+    firebase.auth().signOut()
+      .then(() => {
+        this.setState({
+          email: '',
+          password: '',
+          authenticating: false,
+          user: null,
+        })
+      }, error => {
+        console.error('Sign Out Error', error);
+      });
+  }
+
+  renderCurrentState() {
+    if (this.state.authenticating) {
+      return (
+        <View style={styles.form}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
+    }
+
+    if (this.state.user !== null) {
+      return (
+        <View style={styles.form}>
+          <Text>Logged In</Text>
+          <Button onPress={() => this.onPressLogOut()}>Log Out</Button>
+        </View>
+      )
+    }
+
+    return (
+      <View style={styles.form}>
+        <Input
+          placeholder='Enter your email...'
+          label='Email'
+          onChangeText={email => this.setState({ email })}
+          value={this.state.email}
+        />
+        <Input
+          placeholder='Enter your password...'
+          label='Password'
+          secureTextEntry
+          onChangeText={password => this.setState({ password })}
+          value={this.state.password}
+        />
+        <Button onPress={() => this.onPressSignIn()}>Log In</Button>
+        <Text>{this.state.error}</Text>
+      </View>
+    )
+
   }
 
   render() {
     return (
-      <StyledView>
-        <Login onSubmitName={ this.handleSubmitName } />
-        <StyledText>Open up App.js to start working on your app!</StyledText>
-        <StyledText>Changes you make will automatically reload.</StyledText>
-        <StyledText>Shake your phone to open the developer menu.</StyledText>
-      </StyledView>
+      <View style={styles.container}>
+        {this.renderCurrentState()}
+      </View>
     );
   }
 }
 
-const StyledView = styled.View`
-  flex: 1;
-  background-color: papayawhip;
-  align-items: center;
-  justify-content: center;
-`;
-
-const StyledText = styled.Text`
-  color: palevioletred;
-`;
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row'
+  },
+  form: {
+    flex: 1
+  }
+});
